afd902d88dd15876d592559e12cb036a3bd1f7c7
MegaXChan <megax@megax.cn>
2024-04-24 16:48:34 +0800

    Update Backtrace.h

--- src/Backtrace.h
+++ src/Backtrace.h
@@ -9,34 +9,34 @@
 
 #include "Logger.h"
 
-#if _PREDIXY_BACKTRACE_
+// #if _PREDIXY_BACKTRACE_
 
-#include <execinfo.h>
-inline void traceInfo(int sig)
-{
-#define Size 128
-    logError("predixy backtrace(%d)", sig);
-    void* buf[Size];
-    int num = ::backtrace(buf, Size);
-    int fd = -1;
-    if (Logger::gInst) {
-        fd = Logger::gInst->logFileFd();
-        if (fd >= 0) {
-            backtrace_symbols_fd(buf, num, fd);
-        }
-    }
-    if (fd != STDOUT_FILENO) {
-        backtrace_symbols_fd(buf, num, STDOUT_FILENO);
-    }
-}
+// #include <execinfo.h>
+// inline void traceInfo(int sig)
+// {
+// #define Size 128
+//     logError("predixy backtrace(%d)", sig);
+//     void* buf[Size];
+//     int num = ::backtrace(buf, Size);
+//     int fd = -1;
+//     if (Logger::gInst) {
+//         fd = Logger::gInst->logFileFd();
+//         if (fd >= 0) {
+//             backtrace_symbols_fd(buf, num, fd);
+//         }
+//     }
+//     if (fd != STDOUT_FILENO) {
+//         backtrace_symbols_fd(buf, num, STDOUT_FILENO);
+//     }
+// }
 
-#else
+// #else
 
 inline void traceInfo(int sig)
 {
     logError("predixy backtrace(%d), but current system unspport backtrace", sig);
 }
 
-#endif
+// #endif
 
 #endif
